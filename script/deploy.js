57f3965a9e34dbcbf7e6e68d02df823736cf8cb6
Kefa698 <kefaombongi07@gmail.com>
2023-03-14 15:54:15 +0300

    fixed

--- script/deploy.js
+++ script/deploy.js
@@ -1,17 +1,17 @@
-const { ethers } = require("hardhat");
+const { ethers } = require("hardhat")
 
 async function main() {
-  const OrderBook = await ethers.getContractFactory("OrderBook");
-  const orderBook = await OrderBook.deploy();
+    const OrderBook = await ethers.getContractFactory("OrderBook")
+    const orderBook = await OrderBook.deploy()
 
-  await orderBook.deployed();
+    await orderBook.deployed()
 
-  console.log("OrderBook deployed to:", orderBook.address);
+    console.log("OrderBook deployed to:", orderBook.address)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
